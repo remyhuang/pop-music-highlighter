bfc1d6f6db9f63da07c209c0dd031492492e20cc
Yu-Siang Huang <yshuang@citi.sinica.edu.tw>
2018-10-10 19:55:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def extract(f, length=30, save_score=True, save_thumbnail=True, save_wav=True):
         # score
         attn_score = attn_score / attn_score.max()
         if save_score:
-            np.save('output/score/{}.npy'.format(name), attn_score)
+            np.save('{}_score.npy'.format(name), attn_score)
         
         # thumbnail
         attn_score = attn_score.cumsum()
@@ -31,10 +31,11 @@ def extract(f, length=30, save_score=True, save_thumbnail=True, save_wav=True):
         index = np.argmax(attn_score)
         highlight = [index, index+length]
         if save_thumbnail:
-            np.save('output/highlight/{}.npy'.format(name), highlight)
+            np.save('{}_highlight.npy'.format(name), highlight)
 
         if save_wav:
-            librosa.output.write_wav('output/audio/{}.wav'.format(name), audio[highlight[0]*22050:highlight[1]*22050], 22050)
+            librosa.output.write_wav('{}_audio.wav'.format(name), audio[highlight[0]*22050:highlight[1]*22050], 22050)
 
 if __name__ == '__main__':
+    file_name = 'YOUR MP3 FILE NAME'
     extract(file_name, length=30, save_score=True, save_thumbnail=True, save_wav=True)
